ffac591c5a32cd530258c75c42dff5bc3a89001c
ytcrackers <49995327+ytcrackers@users.noreply.github.com>
2019-04-26 23:43:03 -0400

    Update fplproxies.py

--- fplproxies.py
+++ fplproxies.py
@@ -7,27 +7,37 @@ scrapes 600 http(s) proxies to ip:port
 https://free-proxy-list.net 300
 https://www.us-proxy.org 200
 https://www.sslproxies.org 100
-'''
 
-from requests_html import HTMLSession
+now async! like half a second faster! wow!'''
 
-def z(fpl):
+from requests_html import AsyncHTMLSession
 
-	session = HTMLSession()  # session appears to be currently required for a single get request with this library?
+a = AsyncHTMLSession()  # session appears to be currently required for a single get request with this library?
 
-	cells = session.get(fpl).html.find('td')  # table cells
+async def 一(): return await a.get('https://free-proxy-list.net')
+
+async def 二(): return await a.get('https://www.us-proxy.org')
+
+async def 三(): return await a.get('https://www.sslproxies.org')
+
+results = a.run(一, 二, 三)
+
+for result in results:
+
+	cells = result.html.find('td')
 
 	p = ''  # string as data stream for proxies
 
 	for cell in cells:
+
 		c = cell.text 
+
 		if not c.lower().islower():  # lowercase all letters and then check if islower to determine if the cell contains letters (only ip and port cells will remain)
-			if '.' in c: c = '\n' + c + ':'  # ip will have "." then add newline in front of ip to separate proxies \nip:port\nip:port 
+			
+			if '.' in c:
+
+				c = '\n' + c + ':'  # ip will have "." then add newline in front of ip to separate proxies \nip:port\nip:port 
+			
 			p += c  # string together 
 
 	print(p)
-	
-z('https://free-proxy-list.net')
-z('https://www.us-proxy.org')
-z('https://www.sslproxies.org')
-#z('https://www.socks-proxy.net')  # socks
